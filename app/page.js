3ee8d7a654682d1911c3ee09ea4357adcc2a1801
Amanyadav0007 <bholuyadav11052004@gmail.com>
2026-09-23 02:03:10 +0530

    Blog Item Components added

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
-import BlogItem from "@/Components/BlogItem";
+'use client'
+import BlogList from "@/Components/BlogList";
 import Header from "@/Components/Header";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <BlogItem />
+      <BlogList />
     </>
   );
 }
